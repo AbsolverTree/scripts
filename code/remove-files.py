e644bcfa063da26003048407b65fb7ad89483e95
Gabriel Viterbo <56035644+AbsolverTree@users.noreply.github.com>
2019-12-16 11:35:56 -0300

    Add files via upload

---
+++ code/remove-files.py
@@ -0,0 +1,10 @@
+import os, glob, shutil
+
+path = os.getcwd()
+# print(os.listdir())
+name = input("Nome('*' caso o nome não importe):\n")
+formato = input("Formato(incluir o .):\n")
+files = glob.glob(path+"/"+name+formato)
+
+for i in files:
+    os.unlink(i)
