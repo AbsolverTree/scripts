dcf1336530869ba1e73538519d0db54a837642b9
Gabriel Viterbo <56035644+AbsolverTree@users.noreply.github.com>
2019-12-16 11:37:59 -0300

    Add files via upload

--- code/remove-files.py
+++ code/remove-files.py
@@ -3,7 +3,7 @@ import os, glob, shutil
 path = os.getcwd()
 # print(os.listdir())
 name = input("Nome('*' caso o nome não importe):\n")
-formato = input("Formato(incluir o .):\n")
+formato = input("Formato(incluir o '.' antes):\n")
 files = glob.glob(path+"/"+name+formato)
 
 for i in files:
